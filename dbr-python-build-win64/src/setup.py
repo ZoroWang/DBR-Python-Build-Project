cba4df5fcf1ae52440e64904a5607eb961c84724
Zoro <57085290+ZoroWang@users.noreply.github.com>
2020-01-21 10:28:53 +0800

    Update setup.py

--- dbr-python-build-win64/src/setup.py
+++ dbr-python-build-win64/src/setup.py
@@ -3,11 +3,13 @@ import sys
 import os
 import numpy
 from distutils.command.install import install
+from distutils.sysconfig import get_python_lib
 
 # NumPy header file path.
-numpy_include = os.path.join(os.path.dirname(
-    numpy.__file__), "core", "include", "numpy")
+numpy_include = os.path.join(os.path.dirname(numpy.__file__), "core", "include", "numpy")
 print(numpy_include)
+
+setup_path = os.path.dirname(os.path.realpath(__file__)) 
 
 # Customization for different platforms
 dbr_lib_dir = ''
@@ -17,10 +19,10 @@ dbr_lib_name = 'DynamsoftBarcodeReader'
 
 if sys.platform == "linux" or sys.platform == "linux2":
     # linux
-    dbr_lib_dir = '/usr/lib'
+    dbr_lib_dir = os.path.dirname(os.path.realpath(__file__))
 elif sys.platform == "darwin":
     # OS X
-    dbr_lib_dir = '/usr/lib'
+    dbr_lib_dir = os.path.dirname(os.path.realpath(__file__))
     pass
 elif sys.platform == "win32":
     # Windows
@@ -28,30 +30,46 @@ elif sys.platform == "win32":
     dbr_lib_dir = r'..\lib'
     dbr_dll = r'..\bin'
 
-module_dbr = Extension('dbr', sources=['dbr.c'], include_dirs=[
+if sys.platform == "linux" or sys.platform == "linux2":
+    ext_args = dict(
+        include_dirs = [numpy_include],
+        library_dirs = [dbr_lib_dir],
+        extra_compile_args = ['-std=c99'],
+        extra_link_args = ["-Wl,-rpath=$ORIGIN"],
+        libraries = [dbr_lib_name]
+    )
+elif sys.platform == "darwin":
+    ext_args = dict(
+        include_dirs = [numpy_include],
+        library_dirs = [dbr_lib_dir],
+        extra_compile_args = ['-std=c99'],
+        libraries = [dbr_lib_name]
+    )
+
+
+
+if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
+	module_dbr = Extension('dbr', ['dbr.c'], **ext_args)
+else:
+	module_dbr = Extension('dbr', sources=['dbr.c'], include_dirs=[
                        numpy_include], library_dirs=[dbr_lib_dir], libraries=[dbr_lib_name])
 
 
 class CustomInstall(install):
     def run(self):
         install.run(self)
-        if sys.platform == "win32":
-            import shutil
-            from distutils.sysconfig import get_python_lib
-            src = dbr_dll
+        if sys.platform == "linux" or sys.platform == "linux2":
             dst = get_python_lib()
-
-            if os.path.isdir(src):
-                lst = os.listdir(src)
-                for f in lst:
-                    dll = os.path.join(src, f)
-                    shutil.copy2(dll, dst)
-            else:
-                shutil.copy2(src, dst)
+            lib = setup_path + "/libDynamsoftBarcodeReader.so"
+            os.system("cp {} {}".format(lib, dst))
+        elif sys.platform == "darwin":
+            dst = get_python_lib()
+            lib = setup_path + "/libDynamsoftBarcodeReader.dylib"
+            os.system("cp {} {}".format(lib, dst))
 
 
 setup(name='dbr',
-      version='7.3',
+      version='7.2.2',
       description='Python barcode extension',
       author='Dynamsoft',
       author_email='support@dynamsoft.com',
